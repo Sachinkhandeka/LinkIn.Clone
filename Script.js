b526afb4547ca89c7431691f52241b35206404e1
Sachinkhandeka <136960171+Sachinkhandeka@users.noreply.github.com>
2023-08-05 10:54:50 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,20 @@
+let profileMenu = document.getElementById("profileMenu");
+
+function toggleMenu() {
+    profileMenu.classList.toggle("open-menu");
+}
+// open activity 
+let sidebarActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMore");
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+
+    if(sidebarActivity.classList.contains("open-activity")) {
+        moreLink.innerHTML = "Show less <b>-</b>";
+    } else {
+        moreLink.innerHTML = "Show more <b>+</b>";
+
+    }
+}
+
